25897740466ef7830f19c78d17b74ed39c979f7e
dortiz <dortiz@dortizs-MacBook-Pro.local>
2021-06-11 18:36:04 -0400

    update to home and templates

--- fe/src/views/Settings.js
+++ fe/src/views/Settings.js
@@ -9,7 +9,7 @@ export default class extends JSRouter {
         const header = 'Where can I get some?';
         const msg = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.';
         const html = `
-            <div class="settings">
+            <div class="settings container">
                 <h1>${title}</h1>
                 <h2>${header}</h2>
                 <p>${msg}</p>
